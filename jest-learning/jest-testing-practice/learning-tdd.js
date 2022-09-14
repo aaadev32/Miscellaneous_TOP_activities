b216cd84f6d6c8b1df5f0631fb7f59dd67eee768
zephyr96 <aaadev32@gmail.com>
2022-09-14 12:08:06 -0700

    more tdd stuff

--- jest-learning/jest-testing-practice/learning-tdd.js
+++ jest-learning/jest-testing-practice/learning-tdd.js
@@ -18,7 +18,7 @@ function reverseString(str) {
     console.log(reverseStr);
     return reverseStr;
 }
-let calculator = {
+const calculator = {
     add: function (n1, n2) {
         return n1 + n2;
     }
@@ -31,13 +31,69 @@ let calculator = {
     , divide: function (n1, n2) {
         return n1 / n2;
     }
-
 }
-capitalize('linux');
-reverseString('doodad');
-console.log(calculator.multiply(5, 5));
-console.log(calculator.subtract(5, 2));
-console.log(calculator.divide(5, 3));
 
-module.exports = sum, capitalize, reverseString, calculator.add(), calculator.subtract(), calculator.multiply(), calculator.divide();
+function caesar(str, cipher) {
+    let unicode = [];
+    let encipheredUnicode = [];
+    let result = '';
+    let plaintext = str;
+
+
+
+    //creates unicode array
+    for (let i = 0; i < plaintext.length; i++) {
+
+        unicode[i] = plaintext.charCodeAt(i);
+
+    }
+
+    //creates an enciphered unicode array if the unicode is a lower or uppercase char else the unicode value is unchanged
+    for (let i = 0; i < unicode.length; i++) {
+
+        //this if statement will select lowercase letters for enciphering
+        if (unicode[i] >= 97 && unicode[i] <= 122) {
+
+            encipheredUnicode[i] = unicode[i] + cipher;
+
+
+            while (encipheredUnicode[i] > 122 || encipheredUnicode[i] < 97) {
+
+                if (unicode[i] + cipher > 122) {
+                    encipheredUnicode[i] -= 26;
+                } else if (unicode[i] + cipher < 97) {
+                    encipheredUnicode[i] += 26;
+                }
+            }
+
+            //this else if statement selects uppercase letters for enciphering
+        } else if (unicode[i] >= 65 && unicode[i] <= 90) {
+
+            encipheredUnicode[i] = unicode[i] + cipher;
+
+            while (encipheredUnicode[i] > 90 || encipheredUnicode[i] < 65) {
+                if (unicode[i] + cipher > 90) {
+
+                    encipheredUnicode[i] -= 26;
+                } else if (unicode[i] + cipher < 65) {
+                    encipheredUnicode[i] += 26
+                }
+            }
+        } else {
+            //makes the cipher leave whitespace and punctuation unchanged
+            encipheredUnicode[i] = unicode[i];
+        }
+    }
+
+    for (let i = 0; i < encipheredUnicode.length; i++) {
+        let tempChar = '';
+        let tempConcat = '';
+        tempChar = String.fromCharCode(encipheredUnicode[i]);
+
+        result += tempConcat.concat(tempChar);
+
+    }
+    return result;
+}
+module.exports = { sum, capitalize, reverseString, calculator, caesar };
 
--- jest-learning/jest-testing-practice/learning-tdd.test.js
+++ jest-learning/jest-testing-practice/learning-tdd.test.js
@@ -1,30 +1,51 @@
-const sum = require('./learning-tdd');
-const capitalize = require(`./learning-tdd`);
-const reverseString = require('./learning-tdd');
-const calculator = require('./learning-tdd')
+const testDoc = require('./learning-tdd');
+
+
+
 
 test(`adds 1 + 2 to equal 3`, () => {
-    expect(sum(1, 2)).toBe(3);
+    expect(testDoc.sum(1, 2)).toBe(3);
 });
 
 //this test is failing but after looking through the console.log it works fine....
 test('capitalizes the first character of a string', () => {
-    expect(capitalize(`linux`)).toEqual('Linux')
+    expect(testDoc.capitalize(`linux`)).toBe('Linux');
 });
 
 test('take a string and reverse its characters', () => {
-    expect(reverseString('doodad')).toEqual('dadood')
+    expect(testDoc.reverseString('doodad')).toBe('dadood');
+
+});
+describe('calculator funtion tests', () => {
+    test('calculator object addition function test', () => {
+        expect(testDoc.calculator.add(5, 7)).toBe(12);
+    });
+    test('calculator object subtraction function test', () => {
+        expect(testDoc.calculator.subtract(17, 7)).toBe(10);
+    });
+    test('calculator object multiplication function test', () => {
+        expect(testDoc.calculator.multiply(6, 9)).toBe(54);
+    });
+    test('calculator object division function test', () => {
+        expect(testDoc.calculator.divide(15, 3)).toBe(5);
+    });
 });
 
-test('calculator object addition function test', () => {
-    expect(calculator.add(5, 7)).toBe(12)
-});
-test('calculator object subtraction function test', () => {
-    expect(calculator.subtract(17, 7)).toBe(10)
-});
-test('calculator object multiplication function test', () => {
-    expect(calculator.multiply(6, 9)).toBe(54)
-});
-test('calculator object division function test', () => {
-    expect(calculator.divide(15, 3)).toBe(5)
+describe('caesar function tests', () => {
+    test('caesar function wraps alphabetically correct', () => {
+        expect(testDoc.caesar('zappa', 1)).toBe('abqqb');
+        expect(testDoc.caesar('zappa', -1)).toBe('yzooz');
+    });
+    test('caesar function is case sensitive', () => {
+        expect(testDoc.caesar('fOoBAr', 0)).toBe('fOoBAr');
+    });
+    test('caesar function respects punctuation', () => {
+        expect(testDoc.caesar('don\'t!', 1)).toBe('epo\'u!');
+    });
+    test('works with large shift factors', () => {
+        expect(testDoc.caesar('Hello, World!', 75)).toBe('Ebiil, Tloia!');
+    });
+    test('works with large negative shift factors', () => {
+        expect(testDoc.caesar('Hello, World!', -29)).toBe('Ebiil, Tloia!');
+    });
 });
